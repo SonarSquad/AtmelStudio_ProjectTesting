db48d3a1355b4a2c7b3db68c53067eba49fe763a
MartinCKE <47350854+MartinCKE@users.noreply.github.com>
2020-02-14 11:51:22 +0100

    Rebuild to get PWM out correctly. Not workng yet

--- PWM-testing/main.c
+++ PWM-testing/main.c
@@ -45,17 +45,33 @@ int main(void)
 }
 
 void PWM_Init(void){
-	PORTD_DIR = (PWM1 | PWM2);  //Setting PWM pins as output
+	
+	PORTD_DIR = 0XFF; //Set PORTD as output.
+	
+	
 }
 
 void ClkSelect(void){
 	_PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, 0); //Forces 20MHz CPU clock frequency
+	_PROTECTED_WRITE(CLKCTRL.MCLKCTRLA, (1<<7)); //Sender klokke-signal ut på PA7-pin
+	PORTA_DIR |= (1<<7); //PA7 as output for clock frequency check
 	
 }
 
 void Timer0_Init(void){
-	PORTMUX.TCAROUTEA = PORTMUX_TCA0_PORTD_gc; //Enables PORTMUXD to achieve Waveform Output on the PWM Pins
-	TCA0.SINGLE.CTRLA = (1<<0); //Enables Timer 0
-	TCA0.SINGLE.CTRLB = (1<<4) | (1<<5) | (1<<1); //Sets PWM1 and PWM0 as the waveform output pin and enables frequency generation.
+	PORTMUX.TCAROUTEA |= (PORTMUX_TCA00_bm | PORTMUX_TCA01_bm); //Enables PORTMUXD to achieve Waveform Output on the PWM Pins
+	
+	TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc    /* System Clock */
+	                    | 1 << TCA_SINGLE_ENABLE_bp; /* Module Enable: enabled */
+	TCA0.SINGLE.CTRLB = 0 << TCA_SINGLE_ALUPD_bp            /* Auto Lock Update: disabled */
+	                    | 0 << TCA_SINGLE_CMP0EN_bp         /* Setting: disabled */
+	                    | 0 << TCA_SINGLE_CMP1EN_bp         /* Setting: disabled */
+	                    | 0 << TCA_SINGLE_CMP2EN_bp         /* Setting: disabled */
+	                    | TCA_SINGLE_WGMODE_SINGLESLOPE_gc; /*  */
+	TCA0.SINGLE.CTRLC = 1 << TCA_SINGLE_CMP0OV_bp    /* Setting: enabled */
+		| 1 << TCA_SINGLE_CMP1OV_bp  /* Setting: enabled */
+		| 0 << TCA_SINGLE_CMP2OV_bp; /* Setting: disabled */
+	TCA0.SINGLE.PER = 50;
+	TCA0.SINGLE.CMP0 = 25;  //Dette definerer duty cycle ut på PD0
 	
 }
