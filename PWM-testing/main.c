da999bc04d5764ea539e3e1eb72734fc5cca379b
MartinCKE <47350854+MartinCKE@users.noreply.github.com>
2020-02-25 15:01:48 +0100

    Tried to double frequency. Interrupts occur too often.

--- PWM-testing/main.c
+++ PWM-testing/main.c
@@ -20,8 +20,6 @@
 #define OVF 0
 #define DIR 0
 
-volatile int IntCount = 0;
-
 // Må lage en pinDefines.h som inneholder alle defines for å gjøre alt mere oversiktlig!
 
 
@@ -29,14 +27,14 @@ void PWM_Init(void);
 void ClkSelect(void);
 void Timer0_Init(void);
 
-int lookUp1[] = {
-	33, 62, 33,
+int lookUp1[] = {   //Need 5 clock cycles LESS than PER register. ISR takes 4 clock cycles
+	16, 31, 16,
 	0, 0, 0
 };
 
-int lookUp2[] = {
+int lookUp2[] = {	//Need 5 clock cycles LESS than PER register. ISR takes 4 clock cycles
 	0, 0, 0,
-	33, 56, 33
+	16, 31, 16
 };
 
 
@@ -44,13 +42,13 @@ int lookUp2[] = {
 
 int main(void)
 {
-	sei();  //Global interrupts enabled
+	
 	ClkSelect();
 	Timer0_Init();
 	//TCA0.SPLIT.LCMP0 = 33; /* Compare value Channel 0: 0x20 */
 	//TCA0.SPLIT.HCMP0 = 33; /* Compare value of channel 0: 0x10 */
 	PWM_Init();   
-	
+	sei();  //Global interrupts enabled
 	
     while (1) 
     {
@@ -62,10 +60,10 @@ void PWM_Init(void){
 	
 	PORTD_DIR = 0XFF; //Set PORTD as output.
 	
-	TCA0.SPLIT.LPER = 67; /*  Low-byte Timer Period Register: 0x43 */
-	TCA0.SPLIT.HPER = 67; /*  High-byte Timer Period Register: 0x43 */
+	TCA0.SPLIT.LPER = 37; /*  Low-byte Timer Period Register: 0x43 */
+	TCA0.SPLIT.HPER = 37; /*  High-byte Timer Period Register: 0x43 */
 	
-	TCA0.SINGLE.INTCTRL = (1<<OVF);  //Enables overflow interrupt in Timer 0
+	TCA0.SPLIT.INTCTRL = (1<<OVF);  //Enables overflow interrupt in Timer 0
 }
 
 void ClkSelect(void){
@@ -83,12 +81,9 @@ void Timer0_Init(void){
 	TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc    /* System Clock */
 	                    | 1 << TCA_SPLIT_ENABLE_bp; /* Module Enable: enabled */
 					
-	TCA0.SPLIT.CTRLB = 1 << TCA_SPLIT_HCMP0EN_bp    /* High Compare 0 Enable: enabled */
-	| 1 << TCA_SPLIT_HCMP1EN_bp  /* High Compare 1 Enable: enabled */
-	| 1 << TCA_SPLIT_HCMP2EN_bp  /* High Compare 2 Enable: enabled */
-	| 1 << TCA_SPLIT_LCMP0EN_bp  /* Low Compare 0 Enable: enabled */
-	| 1 << TCA_SPLIT_LCMP1EN_bp  /* Low Compare 1 Enable: enabled */
-	| 1 << TCA_SPLIT_LCMP2EN_bp; /* Low Compare 2 Enable: disabled */
+	TCA0.SPLIT.CTRLB =  TCA_SPLIT_HCMP0EN_bm    /* High Compare 0 Enable: enabled */
+						| 1 << TCA_SPLIT_LCMP0EN_bp;  /* Low Compare 0 Enable: enabled */
+	
 						
 	TCA0.SPLIT.CTRLD = 1 << TCA_SPLIT_SPLITM_bp; /* Split Mode : enabled */
 }
@@ -96,24 +91,19 @@ void Timer0_Init(void){
 
 ISR(TCA0_OVF_vect){
 	
-	//TCA0.SINGLE.INTFLAGS = (1<<OVF) | (1<<4); //clearing the interrupt flag
+	
 	
 	static int num;
 	//TCA0.SINGLE.CMP0 = lookUp1[num];
 	
 	TCA0.SPLIT.LCMP0 = lookUp1[num]; /* Compare value Channel 0: 0x20 */
 	TCA0.SPLIT.HCMP0 = lookUp2[num]; /* Compare value of channel 0: 0x10 */
-	TCA0.SPLIT.LCNT = 67;
-	TCA0.SPLIT.HCNT = 67;
+	TCA0.SPLIT.LCNT = 37;
+	TCA0.SPLIT.HCNT = 37;
 	
 	if(++num >= 6){ // Pre-increment num then check it's below 10.
 		num = 0;       // Reset num.
-
 	}
 	
-	//IntCount++;
-	
-	
-
-	
+	TCA0.SPLIT.INTFLAGS = (1<<OVF) | (1<<4); //clearing the interrupt flag
 }
